40a4eddf31e5b0149e425ca94c8086a036eaafa8
ILOANI <chinweiloani@yahoo.com>
2022-07-18 22:23:18 +0300

    index page update

--- front/js/index.js
+++ front/js/index.js
@@ -1,4 +1,4 @@
-//------------------------------------------------------------------------
+
 // Récupération des produits de l'api
 //------------------------------------------------------------------------ 
 fetch("http://localhost:3000/api/products")
@@ -6,39 +6,35 @@ fetch("http://localhost:3000/api/products")
   // changer la réponse reçue en json
   .then((res) => res.json())
 
-  // ce qui a été reçu et qui a été traité en json sera appelé productObjects
+  // ce qui a été reçu et qui a été traité en json 
   .then((productObjects) => {
 
-    // pour obtenir des informations dans la console sur ce qui est récupéré en tant que tableau.
+  // pour obtenir des informations dans la console.
     console.table(productObjects);
 
-    // pour appeler la fonction d'affichage du produit.
-    lesKanaps(productObjects);
+  // pour appeler la fonction d'affichage du produit.
+    kanap(productObjects);
   })
 
-  // dans le cas d'une erreur remplace le contenu de titre par un h1 au contenu de erreur 404 et renvoit en console l'erreur.
+  // dans le cas d'une erreur 
   .catch((err) => {
     document.querySelector(".titles").innerHTML = "<h1>erreur 404</h1>";
     console.log("erreur 404, sur ressource api:" + err);
   });
 
     //affichage des produits api sur la page d'index
-function lesKanaps(index) {
+function kanap(index) {
 
   // déclaration de variable de la zone d'article
-  let zoneArticle = document.querySelector("#items");
+  let articleZone = document.querySelector("#items");
 
   // boucle pour chaque indice(nommé 'article') dans index
-  for (let article of index) {
-
-    /* création et ajout des zones d'articles, insertion de l'adresse produit via chemin produit + paramètres(son id);
-    la page index est http://127.0.0.1:5500/front/html/index.html donc la page du produit sera http://127.0.0.1:5500/front/html/product.html 
-    (d'ou le ./product.html) pour rajouter son paramètre on met ? puis la clé (ici _id) associé (=) à sa valeur dynamique ${article._id} */
-    zoneArticle.innerHTML += `<a href="./product.html?_id=${article._id}">
+  for (let product of index) {
+    articleZone.innerHTML += `<a href="./product.html?_id=${product._id}">
     <article>
-      <img src="${article.imageUrl}" alt="${article.altTxt}">
-      <h3 class="productName">${article.name}</h3>
-      <p class="productDescription">${article.description}</p>
+      <img src="${product.imageUrl}" alt="${product.altTxt}">
+      <h3 class="productName">${product.name}</h3>
+      <p class="productDescription">${product.description}</p>
     </article>
   </a>`;
   }
